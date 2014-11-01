a80b473de89a73750a991bd23ed649c15656c1f6
Dineshrajaa <dineshrajaa.93@gmail.com>
2014-11-01 16:10:31 +0530

    Started Task 8 added references for previous Tasks

---
+++ Task8/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Admin Panel</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">	
+	<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+</head>
+	<body>
+		
+	</body>
+</html>
